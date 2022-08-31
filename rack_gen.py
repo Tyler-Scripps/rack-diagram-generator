4d8340935507ae0dc286f94866b6b2c92cc5dfa3
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-08-31 01:37:36 -0600

    Basic switch and storage workng

--- rack_gen.py
+++ rack_gen.py
@@ -2,7 +2,8 @@ import sys
 import json
 from tkinter.ttk import Style
 import svgwrite
-# from svgwrite import inch
+import math
+
 
 def createRack(rackObj):
     rackGroup = dwg.add(dwg.g(id="rack", fill="#252525"))
@@ -47,18 +48,78 @@ def createRack(rackObj):
         rackHoles.add(line1)
         rackHoles.add(line2)
 
+def createEthernet(x, y):
+    tempGroup = dwg.add(dwg.g())
+    tempGroup.add(dwg.rect(insert=(str(x-0.3149606)+"in", str(y-0.265748)+"in"), size=("16mm","13.5mm"), fill="#444444"))
+    tempGroup.add(dwg.rect(insert=(str(x-0.2358268)+"in", str(y-0.1771654)+"in"), size = ("11.6mm", "9mm")))
+    tempGroup.add(dwg.rect(insert=(str(x-0.125)+"in", str(y-0.03937008+0.1771654)+"in"), size=("6.35mm", "2mm")))
 
+def createDriveLFF(x,y):
+    tempGroup = dwg.add(dwg.g())
+    tempGroup.add(dwg.rect(insert=(str(x-2)+"in", str(y-.25)+"in"), size=("4in", "1in"), fill="#666666"))
+
+def createNotes(obj, rackSize):
+    height = ((rackSize - obj["position"]) * 1.75) + 1
+    text = ""
+    if "name" in obj:
+        text += "Name: " + obj["name"] + ", "
+    if "ip" in obj:
+        text += "ip: " + obj["ip"] + ", "
+    g=dwg.g(style="font-size: .75in; stroke: white; fill: black; font-family: Arial")
+    g.add(dwg.text(text, insert=("20.25in",str(height + 1)+"in")))
+    dwg.add(g)
 
 def createSwitch(switchObj, rackSize):
     if switchObj["type"] != "switch":
-        return 0
+        return False
     tempGroup = dwg.add(dwg.g(id=switchObj["name"]))
     height = ((rackSize - switchObj["position"]) * 1.75) + 1
-    tempGroup.add(dwg.rect(insert=(".5in",str(height)+"in"), size=("19in",str(switchObj["size"]*1.75)+"in")))
-    tempGroup.add(dwg.circle(center=(".8125in",str(height+.25)+"in"), r=".1in", fill="#555555"))
-    tempGroup.add(dwg.circle(center=(".8125in",str(height+1.5)+"in"), r=".1in", fill="#555555"))
-    tempGroup.add(dwg.circle(center=("19.1875in",str(height+.25)+"in"), r=".1in", fill="#555555"))
-    tempGroup.add(dwg.circle(center=("19.1875in",str(height+1.5)+"in"), r=".1in", fill="#555555"))
+    tempGroup.add(dwg.rect(insert=(".5in",str(height)+"in"), size=("19in",str(switchObj["size"]*1.75)+"in")))   #body rectangle
+    tempGroup.add(dwg.circle(center=(".8125in",str(height+.25)+"in"), r=".1in", fill="#555555"))    #top left screw
+    tempGroup.add(dwg.circle(center=(".8125in",str(height+(1.5 + ((switchObj["size"] - 1) * 1.75)))+"in"), r=".1in", fill="#555555"))    #bottom left screw
+    tempGroup.add(dwg.circle(center=("19.1875in",str(height+.25)+"in"), r=".1in", fill="#555555"))  #top right screw
+    tempGroup.add(dwg.circle(center=("19.1875in",str(height+(1.5 + ((switchObj["size"] - 1) * 1.75)))+"in"), r=".1in", fill="#555555"))  #bottom right screw
+    for i in range(switchObj["ports"]):
+        if (i % 2) == 0:    #even
+            x = 18 - (.7 * (i/2))
+            y = height + .55
+            createEthernet(x, y)
+        else:   #odd
+            x = 18 - (.7 * math.trunc((i/2)))
+            y = height + 1.2
+            createEthernet(x, y)
+    createNotes(switchObj, rackSize)
+    return True
+
+def createStorage(storageObj, rackSize):
+    if storageObj["type"] != "storage":
+        return False
+    tempGroup = dwg.add(dwg.g(id=storageObj["name"]))
+    height = ((rackSize - storageObj["position"]) * 1.75) + 1
+    tempGroup.add(dwg.rect(insert=(".5in",str(height)+"in"), size=("19in",str(storageObj["size"]*1.75)+"in")))   #body rectangle
+    tempGroup.add(dwg.circle(center=(".8125in",str(height+.25)+"in"), r=".1in", fill="#555555"))    #top left screw
+    tempGroup.add(dwg.circle(center=(".8125in",str(height+(1.5 + ((storageObj["size"] - 1) * 1.75)))+"in"), r=".1in", fill="#555555"))    #bottom left screw
+    tempGroup.add(dwg.circle(center=("19.1875in",str(height+.25)+"in"), r=".1in", fill="#555555"))  #top right screw
+    tempGroup.add(dwg.circle(center=("19.1875in",str(height+(1.5 + ((storageObj["size"] - 1) * 1.75)))+"in"), r=".1in", fill="#555555"))  #bottom right screw
+    if storageObj["driveSize"] == 3.5:
+        for i in range(storageObj["drives"]):
+            y = height + .375 + (math.trunc(i/4)*1.125)
+            if i%4 == 0:
+                x = 16.525
+                createDriveLFF(x,y)
+            elif i%4 == 1:
+                x = 12.175
+                createDriveLFF(x,y)
+            elif i%4 == 2:
+                x = 7.825
+                createDriveLFF(x,y)
+            elif i%4 == 3:
+                x = 3.475
+                createDriveLFF(x,y)
+    elif storageObj["driveSize"] == 2.5:
+        pass
+    createNotes(storageObj, rackSize)
+    return True
 
 
 if len(sys.argv) != 2:
@@ -68,7 +129,7 @@ if len(sys.argv) != 2:
 f = open(sys.argv[1])
 data = json.load(f)
 f.close()
-print(data)
+# print(data)
 
 # print(data['rack']['size'])
 
@@ -91,5 +152,7 @@ if "servers" in  data:
         print(server)
         if server["type"] == "switch":
             createSwitch(server, size)
+        elif server["type"] == "storage":
+            createStorage(server, size)
 
 dwg.save()
